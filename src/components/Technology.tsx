30c91a8fb78ba61b3669c984c5b0e907144f71b7
achiewe <mamukashviliachi706@gmail.com>
2023-05-25 20:24:54 +0400

    add image landscape and portrait

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import BgMobile from "../../public/photoes/technology/background-technology-mobile.jpg";
 import BgTablet from "../../public/photoes/technology/background-technology-tablet.jpg";
+import BgDesktop from "../../public/photoes/technology/background-technology-desktop.jpg";
 import Header from "./Header/Header";
 import { useParams, Link } from "react-router-dom";
 import data from "../../data.json";
@@ -29,7 +30,8 @@ const Technology = (): JSX.Element => {
       (item) => item.name === "Space capsule"
     );
   }
-  const imageAddress = `.${TechnologyInfo?.images?.landscape}`;
+  const imageLandscape = `.${TechnologyInfo?.images?.landscape}`;
+  const imagePortrait = `.${TechnologyInfo?.images?.portrait}`;
   return (
     <TechnologyMain>
       <div className="header-div">
@@ -40,24 +42,31 @@ const Technology = (): JSX.Element => {
         <p className="space-text"> SPACE LAUNCH 101</p>
       </div>
       <div className="main-technology">
-        <img src={imageAddress} alt="spacecraft image" />
-        <NavDiv>
-          <Link to="/Technology/vehicle" className="vehicle">
-            1
-          </Link>
-          <Link to="/Technology/spaceport" className="spaceport">
-            2
-          </Link>
-          <Link to="/Technology/capsule" className="capsule">
-            3
-          </Link>
-        </NavDiv>
-        <div className="terminology-main">
-          <div className="terminology-name">
-            <h2>THE TERMINOLOGY… </h2>
-            <h1> {TechnologyInfo?.name}</h1>
+        <img
+          className="landscape"
+          src={imageLandscape}
+          alt="spacecraft image"
+        />
+        <img className="portrait" src={imagePortrait} alt="spacecraft image" />
+        <div className="bottom-div">
+          <NavDiv>
+            <Link to="/Technology/vehicle" className="vehicle">
+              1
+            </Link>
+            <Link to="/Technology/spaceport" className="spaceport">
+              2
+            </Link>
+            <Link to="/Technology/capsule" className="capsule">
+              3
+            </Link>
+          </NavDiv>
+          <div className="terminology-main">
+            <div className="terminology-name">
+              <h2>THE TERMINOLOGY… </h2>
+              <h1> {TechnologyInfo?.name}</h1>
+            </div>
+            <p>{TechnologyInfo?.description} </p>
           </div>
-          <p>{TechnologyInfo?.description} </p>
         </div>
       </div>
     </TechnologyMain>
@@ -78,6 +87,11 @@ const TechnologyMain = styled.div`
   @media (min-width: 768px) {
     background-image: url(${BgTablet});
     padding-bottom: 98px;
+  }
+
+  @media (min-width: 1024px) {
+    background-image: url(${BgDesktop});
+    padding-bottom: 120px;
   }
 
   .header-div {
@@ -104,6 +118,11 @@ const TechnologyMain = styled.div`
       margin-top: 64px;
       margin-bottom: 60px;
     }
+    @media (min-width: 1024px) {
+      padding-left: 166.5px;
+      margin-top: 100px;
+      margin-bottom: 137px;
+    }
 
     h3 {
       font-family: "Barlow Condensed";
@@ -119,6 +138,14 @@ const TechnologyMain = styled.div`
         font-weight: 700;
         line-height: 24px;
         letter-spacing: 3.375px;
+        text-align: left;
+      }
+
+      @media (min-width: 1024px) {
+        font-size: 28px;
+        font-weight: 700;
+        line-height: 34px;
+        letter-spacing: 4.724999904632568px;
         text-align: left;
       }
     }
@@ -139,6 +166,14 @@ const TechnologyMain = styled.div`
         letter-spacing: 3.375px;
         text-align: left;
       }
+
+      @media (min-width: 1024px) {
+        font-size: 28px;
+        font-weight: 400;
+        line-height: 34px;
+        letter-spacing: 4.724999904632568px;
+        text-align: left;
+      }
     }
   }
 
@@ -153,85 +188,148 @@ const TechnologyMain = styled.div`
       gap: 45px;
     }
 
-    img {
+    @media (min-width: 1024px) {
+      flex-direction: row-reverse;
+      justify-content: space-between;
+    }
+
+    .landscape {
       width: 100%;
       height: 170px;
       @media (min-width: 768px) {
         height: 310px;
       }
+      @media (min-width: 1024px) {
+        display: none;
+      }
     }
 
-    .terminology-main {
+    .portrait {
+      display: none;
+      width: 100%;
+      @media (min-width: 1024px) {
+        display: block;
+        max-width: 515px;
+        height: 527px;
+      }
+    }
+
+    .bottom-div {
       width: 100%;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      gap: 16px;
+      gap: 26px;
+      @media (min-width: 768px) {
+        gap: 44px;
+      }
+      @media (min-width: 1024px) {
+        flex-direction: row;
+        gap: 80px;
+      }
 
-      .terminology-name {
+      .terminology-main {
         width: 100%;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        gap: 9px;
-        @media (min-width: 768px) {
-          gap: 16px;
+        gap: 16px;
+        @media (min-width: 1024px) {
+          gap: 17px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          max-width: 470px;
         }
 
-        h2 {
-          font-family: "Barlow Condensed";
-          font-size: 14px;
+        .terminology-name {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 9px;
+          @media (min-width: 768px) {
+            gap: 16px;
+          }
+
+          @media (min-width: 1024px) {
+            gap: 11px;
+            justify-content: flex-start;
+            align-items: flex-start;
+          }
+
+          h2 {
+            font-family: "Barlow Condensed";
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 17px;
+            letter-spacing: 2.362499952316284px;
+            text-align: center;
+            color: #d0d6f9;
+            @media (min-width: 768px) {
+              font-size: 16px;
+              font-weight: 400;
+              line-height: 19px;
+              letter-spacing: 2.700000047683716px;
+              text-align: left;
+            }
+          }
+
+          h1 {
+            font-family: "Bellefair";
+            font-size: 24px;
+            color: #ffffff;
+            font-weight: 400;
+            line-height: 28px;
+            letter-spacing: 0px;
+            text-align: center;
+            text-transform: uppercase;
+            @media (min-width: 768px) {
+              font-size: 40px;
+              font-weight: 400;
+              line-height: 46px;
+              letter-spacing: 0px;
+              text-align: center;
+            }
+
+            @media (min-width: 1024px) {
+              font-size: 56px;
+              font-weight: 400;
+              line-height: 64px;
+              letter-spacing: 0px;
+              text-align: left;
+            }
+          }
+        }
+
+        p {
+          font-family: "Barlow";
+          font-size: 15px;
           font-weight: 400;
-          line-height: 17px;
-          letter-spacing: 2.362499952316284px;
+          line-height: 25px;
+          letter-spacing: 0px;
           text-align: center;
           color: #d0d6f9;
+          width: 327px;
           @media (min-width: 768px) {
             font-size: 16px;
             font-weight: 400;
-            line-height: 19px;
-            letter-spacing: 2.700000047683716px;
-            text-align: left;
-          }
-        }
-
-        h1 {
-          font-family: "Bellefair";
-          font-size: 24px;
-          color: #ffffff;
-          font-weight: 400;
-          line-height: 28px;
-          letter-spacing: 0px;
-          text-align: center;
-          text-transform: uppercase;
-          @media (min-width: 768px) {
-            font-size: 40px;
-            font-weight: 400;
-            line-height: 46px;
+            line-height: 28px;
             letter-spacing: 0px;
             text-align: center;
+            width: 458px;
           }
-        }
-      }
 
-      p {
-        font-family: "Barlow";
-        font-size: 15px;
-        font-weight: 400;
-        line-height: 25px;
-        letter-spacing: 0px;
-        text-align: center;
-        color: #d0d6f9;
-        width: 327px;
-        @media (min-width: 768px) {
-          font-size: 16px;
-          font-weight: 400;
-          line-height: 28px;
-          letter-spacing: 0px;
-          text-align: center;
-          width: 458px;
+          @media (min-width: 1024px) {
+            font-size: 18px;
+            font-weight: 400;
+            line-height: 32px;
+            letter-spacing: 0px;
+            text-align: left;
+            width: 444px;
+          }
         }
       }
     }
@@ -245,6 +343,12 @@ const NavDiv = styled.nav`
   justify-content: center;
   gap: 16px;
   align-items: center;
+
+  @media (min-width: 1024px) {
+    flex-direction: column;
+    max-width: 80px;
+    gap: 32px;
+  }
 
   a {
     width: 40px;
@@ -269,6 +373,16 @@ const NavDiv = styled.nav`
       font-weight: 400;
       line-height: 28px;
       letter-spacing: 1.5px;
+      text-align: center;
+    }
+
+    @media (min-width: 1024px) {
+      width: 80px;
+      height: 80px;
+      font-size: 32px;
+      font-weight: 400;
+      line-height: 37px;
+      letter-spacing: 2px;
       text-align: center;
     }
   }
